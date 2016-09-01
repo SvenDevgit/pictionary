bf0cfe8ca8f3e568fa2e6a6f841fd4554d4679cd
SvenDevgit <svenlambeck@hotmail.com>
2016-09-01 11:18:01 +0200

    extra feature commit

--- public/main.js
+++ public/main.js
@@ -1,13 +1,32 @@
-var pictionary = function() {
+var socket = io();
+var gameRole = 'Guesser';  
+var setRole = function(connectionCount){
+    console.log('connection count in set role '  + connectionCount);
+      if(connectionCount == 1){
+         gameRole = 'Drawer';
+         console.log('we have a drawer');  
+      }
+      $('#role').text('your role is ' + gameRole);  
+   }; 
+
+var pictionary = function(connectionCount) {
+   setRole(connectionCount); 
+   if (gameRole == 'Drawer'){
+      socket.emit('setword');
+   } 
+   console.log('in pictionary function gameRole ' + gameRole);
    var canvas, context;
-   var socket = io();
    var mousedown = false;
-   var guessBox;
-   
+   var guessBox 
    var showGuess = function(guess) {
       console.log(guess);
       $('#lastguess').text('last guess ' +  guess.toLowerCase());
    };
+
+   var setWord = function(gameWord){
+    console.log('in the setword function');
+      $('#gameword').text('the word for this game is ' + gameWord); 
+   }
 
    var onKeyDown = function(event) {
       if (event.keyCode != 13) { // Enter
@@ -18,6 +37,14 @@ var pictionary = function() {
       socket.emit('guess', guess);
       showGuess(guess);
       guessBox.val('');
+   };
+
+   var connectionMessage = function(connectionCount){
+      console.log('connection count ' + connectionCount);
+      if (connectionCount == 1){
+        console.log('there is only one player wait for new players');
+      }
+
    };
 
    guessBox = $('#myguess input');
@@ -33,28 +60,35 @@ var pictionary = function() {
    context = canvas[0].getContext('2d');
    canvas[0].width = canvas[0].offsetWidth;
    canvas[0].height = canvas[0].offsetHeight;
-   canvas.on('mousedown', function() {
-      mousedown = true;
-      return mousedown;
-   });
-   canvas.on('mouseup', function() {
-      mousedown = false;
-    return mousedown;
-   });
-   canvas.on('mousemove', function(event) {
-      if (mousedown){  
-         var offset = canvas.offset();
-         var position = {x: event.pageX - offset.left,
-                        y: event.pageY - offset.top};
-         socket.emit('draw', position);                
-         draw(position);
-       }  
-    });
 
+   if (gameRole == 'Drawer' ){
+      canvas.on('mousedown', function() {
+         mousedown = true;
+         return mousedown;
+      });
+      canvas.on('mouseup', function() {
+         mousedown = false;
+         return mousedown;
+      });
+      canvas.on('mousemove', function(event) {
+         if (mousedown){  
+            var offset = canvas.offset();
+            var position = {x: event.pageX - offset.left,
+                           y: event.pageY - offset.top};
+            socket.emit('draw', position);                
+            draw(position);
+          }  
+      });
+   }
    socket.on('draw', draw);
    socket.on('guess', showGuess);
+   if (gameRole == 'Drawer'){
+      socket.on('setword', setWord);
+   }  
+  socket.on('disconnect', connectionMessage);
 };
 
 $(document).ready(function() {
-    pictionary();
+    socket.on('concount', pictionary);
+    //pictionary();
 });
--- server.js
+++ server.js
@@ -8,8 +8,41 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+var connectionCount = 0;
+
+var WORDS = [
+    "word", "letter", "number", "person", "pen", "class", "people",
+    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "brother", "sister", "world", "head", "page", "country", "question",
+    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+    "west", "child", "children", "example", "paper", "music", "river", "car",
+    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+    "space"
+];
+
+function chooseRandomword(){
+   var id = Math.floor((Math.random() * 100) - 1);
+   console.log('id ' + id);
+   var randomWord = WORDS[id];
+   console.log('random word ' + randomWord);
+   return randomWord;
+};
+
+console.log('words length ' + WORDS.length);
+
 io.on('connect', function(socket) {
    console.log('client connected');
+
+   connectionCount++;
+   io.emit('concount', connectionCount);
+   //io.emit('setWord', chooseRandomword());
+
 
    socket.on('draw', function(position) {
       socket.broadcast.emit('draw', position);
@@ -18,7 +51,19 @@ io.on('connect', function(socket) {
    socket.on('guess', function(guess) {
       socket.broadcast.emit('guess', guess);
    });
-   
+
+   socket.on('setword', function(word){
+   	console.log('in de setword listener');
+
+   	  io.emit('setword',chooseRandomword());
+   });
+
+   socket.on('disconnect', function() {
+      console.log('A user has disconnected');
+      connectionCount--;
+      io.emit('disconnect', connectionCount);
+   });
+
 });    
 
 server.listen(process.env.PORT || 8080);
